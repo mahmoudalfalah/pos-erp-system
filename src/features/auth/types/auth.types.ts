8950a1688c1e3e63fb7771875b80d27bd6531aaf
MahmoudGEA2005 <mahmoud.ea2005@gmail.com>
2026-05-26 13:38:06 +0300

    feat(auth): add general types to auth

--- src/features/auth/types/auth.types.ts
+++ src/features/auth/types/auth.types.ts
@@ -1,3 +1,13 @@
+import type { Role } from "@/generated/prisma";
+
 export type LoginActionState = {
     error?: string;
+}
+
+export type SessionUser = {
+    id: string;
+    name: string;
+    email: string;
+    avatar?: string;
+    role: Role;
 }
