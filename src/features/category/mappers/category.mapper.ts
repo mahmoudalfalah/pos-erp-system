eafa35f3c9852ef0c5ea0bc331170370ec4f39f5
Mahmoud Alfalah <mahmoud@malfalah.com>
2026-07-21 23:31:03 +0300

    feat(category): Implement CategoryDTO and serialization mapper (#65)
    Refs #29

--- src/features/category/mappers/category.mapper.ts
+++ src/features/category/mappers/category.mapper.ts
@@ -1,5 +1,6 @@
 import type { Category as PrismaCategory } from "@/generated/prisma";
 import type { Category } from "../types/category.type";
+import type { CategoryDTO } from "../dtos/category.dto";
 
 export const CategoryMapper = {
     toDomain(raw: PrismaCategory): Category {
@@ -11,6 +12,13 @@ export const CategoryMapper = {
             isActive: raw.isActive,
             createdAt: raw.createdAt,
             updatedAt: raw.updatedAt,
+        };
+    },
+    toDto(domain: Category): CategoryDTO {
+        return {
+            ...domain,
+            createdAt: domain.createdAt.toISOString(),
+            updatedAt: domain.updatedAt.toISOString(),
         };
     }
 }
