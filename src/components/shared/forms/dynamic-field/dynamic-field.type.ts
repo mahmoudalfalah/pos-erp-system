81cf316d117931693780272c8d4a56f0b3b18d0b
Mahmoud Alfalah <mahmoud@malfalah.com>
2026-08-19 15:00:16 +0300

    feat(ui): extract DynamicField component for form field mapping (#106)
    Refs #105

---
+++ src/components/shared/forms/dynamic-field/dynamic-field.type.ts
@@ -0,0 +1,11 @@
+import type { InputHTMLAttributes, ElementType } from 'react';
+import type { UseFormProps } from 'react-hook-form';
+
+export type DynamicFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+    id: string;
+    label: string;
+    placeholder: string;
+    errorMessage?: string;
+    registerProps: UseFormProps;
+    component: ElementType;
+};
