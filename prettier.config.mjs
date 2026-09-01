7c6242e7dac975b4b3523d253420e9a9f3b2cf98
Mahmoud Alfalah <mahmoud@malfalah.com>
2026-09-01 13:17:35 +0300

    refactor(prettier): migrate configuration to mjs (#133)
    Refs #132

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+export default {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    jsxSingleQuote: false,
+    printWidth: 100,
+    tabWidth: 4,
+    endOfLine: 'lf',
+};
